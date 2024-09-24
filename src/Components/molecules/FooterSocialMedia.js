2313fc2be8b13419b46d822f832dd974119a5f99
lillysky07 <deliaputriadel096@gmail.com>
2024-09-24 11:25:54 +0700

    revisi footer

--- src/Components/molecules/FooterSocialMedia.js
+++ src/Components/molecules/FooterSocialMedia.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import SocialIcon from '../atoms/SocialIcon';
 import './FooterSocialMedia.css';
+import facebook from '../image/facebook.png';
+import icon1 from '../image/discord.png';
 
 const SocialMedia = () => {
   return (
     <div className="social-icons">
-      <SocialIcon link="https://www.facebook.com" imgSrc="/facebook.png" altText="Facebook" />
-      <SocialIcon link="https://www.dribble.com" imgSrc="/dribbble.png" altText="Dribbble" />
-      <SocialIcon link="https://www.behance.com" imgSrc="/behance.png" altText="Behance" />
-      <SocialIcon link="https://www.instagram.com" imgSrc="/instagram.png" altText="Instagram" />
-      <SocialIcon link="https://www.discord.com" imgSrc="/discord.png" altText="Discord" />
+      <SocialIcon link="https://www.facebook.com" imgSrc={facebook} altText="Facebook" />
+      <SocialIcon link="https://www.dribble.com" imgSrc={icon1} altText="Dribbble" />
+      <SocialIcon link="https://www.behance.com" imgSrc={facebook} altText="Behance" />
+      <SocialIcon link="https://www.instagram.com" imgSrc={facebook} altText="Instagram" />
+      <SocialIcon link="https://www.discord.com" imgSrc={facebook} altText="Discord" />
     </div>
   );
 };
